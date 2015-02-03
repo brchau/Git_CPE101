3ad58684bf856ef061a5724f6f1ce1f8e819f62a
brchau <brianchau0@gmail.com>
2015-02-02 21:54:59 -0800

    Lab1 Test

---
+++ lab1.py
@@ -0,0 +1,11 @@
+#
+# This is a header block example for lab 1.
+#
+# You will need to supply the following information.
+#
+# Name:Brian Chau
+# Instructor:Aaron Keen
+# Section:9
+#
+
+print ("Hello, Brian Chau.")
